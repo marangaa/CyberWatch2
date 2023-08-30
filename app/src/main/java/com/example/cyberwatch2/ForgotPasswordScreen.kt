6d7afb37bcca9b2f922101c367fadf917869079a
Maranga <rchdmaranga@gmail.com>
2023-08-30 19:17:36 +0300

    Added a logo or app name at the top to provide a visual identifier. Enhanced the email and password input fields with elevation for a subtle 3D effect. Added subtle elevation to the buttons for a button press effect. Included animations for the buttons to provide visual feedback. Adjusted padding and spacing to improve overall layout and readability.

--- app/src/main/java/com/example/cyberwatch2/ForgotPasswordScreen.kt
+++ app/src/main/java/com/example/cyberwatch2/ForgotPasswordScreen.kt
@@ -1,22 +1,13 @@
 package com.example.cyberwatch2
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseAuth
 
@@ -36,7 +27,13 @@ fun ForgotPasswordScreen() {
         OutlinedTextField(
             value = email,
             onValueChange = { email = it },
-            label = { Text("Email") }
+            label = { Text("Email") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Gray,
+                unfocusedBorderColor = Color.LightGray
+            ),
+            modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
@@ -51,13 +48,20 @@ fun ForgotPasswordScreen() {
                             }
                         }
                 }
-            }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp)
         ) {
             Text("Reset Password")
         }
         Spacer(modifier = Modifier.height(16.dp))
         if (showMessage) {
-            Text("Password reset email sent.")
+            Text(
+                text = "Password reset email sent. Please check your inbox.",
+                color = Color.Green,
+                modifier = Modifier.padding(top = 8.dp)
+            )
         }
     }
 }
